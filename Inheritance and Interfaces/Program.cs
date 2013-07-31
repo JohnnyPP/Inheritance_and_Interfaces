b014c5e381e08a7d951c54d19e8c86f8daf6f548
JohnnyPP <johnnnyppp@gmail.com>
2013-07-31 23:27:09 +0200

    Initial release up to:
    http://youtu.be/naJQVlTmHdU?t=4m5s

---
+++ Inheritance and Interfaces/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Interfaces
+{
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("I can speak!");
+        }
+        
+    }
+
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Dog");
+        }
+        
+    }
+
+
+    class Pitbull : Dog
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Pitbull");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Cat");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started!");
+
+            Animal a = new Cat();
+            a.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
